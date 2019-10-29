61dd7c6ee1a907f98f16938207f50c1fb69c619d
tfranks247 <tfranks247@gmail.com>
2019-10-29 11:06:29 -0400

    first push

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String [] nameArray = {"Tony", "Sam", "Hunter", "Bob"};
+
+        for(int i = 0 ; i < nameArray.length; i++)
+        {
+            String name = nameArray[i];
+            System.out.println(name);
+        }
+    }
+}
